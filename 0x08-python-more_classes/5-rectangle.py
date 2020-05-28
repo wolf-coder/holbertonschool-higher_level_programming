49db67a1cc948c7fc7e9e3432a668b84048f89e6
wolf-coder <cuore.fakhri@gmail.com>
2020-05-28 07:55:47 +0100

    Adding __del__ method

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python3
+
+"""
+An empty class Rectangle
+"""
+
+
+class Rectangle:
+    """
+    Empty class Rectangle
+    """
+    def __init__(self, width=0, height=0):
+        """
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        """
+        if isinstance(value, int):
+            if value < 0:
+                raise ValueError("width must be >= 0")
+            else:
+                self.__width = value
+        else:
+            raise TypeError("width must be an integer")
+
+    @property
+    def height(self):
+        """
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        """
+        if isinstance(value, int):
+            if value < 0:
+                raise ValueError("height must be >= 0")
+            else:
+                self.__height = value
+        else:
+            raise TypeError("height must be an integer")
+
+    def area(self):
+        """
+        """
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """
+        """
+        if (self.__height * self.__width) is 0:
+            return 0
+        return (self.__height + self.__width) * 2
+
+    def __str__(self):
+        """
+        """
+        if (self.__height * self.__width) is 0:
+            return ""
+        else:
+            Str = ""
+            for i in range(self.__height):
+                for j in range(self.__width):
+                    Str += "#"
+                if i < self.__height - 1:
+                    Str += "\n"
+            return Str
+
+    def __repr__(self):
+        """
+        """
+        return "Rectangle(" + str(self.__width) + ", "\
+            + str(self.__height) + ")"
+
+    def __del__(self):
+        """
+        """
+        print("Bye rectangle...")
