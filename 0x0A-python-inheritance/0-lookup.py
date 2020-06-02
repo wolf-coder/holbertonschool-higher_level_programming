17dc83161bcd368a589bbc7afb769b869d1292aa
wolf-coder <cuore.fakhri@gmail.com>
2020-06-02 19:22:51 +0100

    Fixing output

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 """
+Module
 """
 
 
 def lookup(obj):
     """
+    function that returns the list of available
+    attributes and methods of an object.
+
+    args:
+    obj(Object)
     """
     return dir(obj)
