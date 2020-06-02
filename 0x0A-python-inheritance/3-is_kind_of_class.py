1c4a3d91a4dcd8feeb6bf3725d2f4d96ee35de8c
wolf-coder <cuore.fakhri@gmail.com>
2020-06-02 22:20:19 +0100

    Checking whether the object is an

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Class mudule
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    function that returns True if the object
+    is an instance of, or if the object is an
+    instance of a class that inherited from, the
+    specified class ; otherwise False.
+    """
+    return isinstance(obj, a_class)
