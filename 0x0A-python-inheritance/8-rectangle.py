1b8d1b09d0940267451b88f2eb3532d7df054f25
wolf-coder <cuore.fakhri@gmail.com>
2020-06-02 23:12:16 +0100

    fix output

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -33,7 +33,7 @@ class Rectangle(BaseGeometry):
         """
         Rectangle sublass of BaseGeometry
         """
-        self.integer_validator("width", width)
+        super().integer_validator("width", width)
         self.__width = width
-        self.integer_validator("height", height)
+        super().integer_validator("height", height)
         self.__height = height
