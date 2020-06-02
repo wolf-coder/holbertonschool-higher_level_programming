df866418a0bc9785c5ba87fa36f090a5feb0fed5
wolf-coder <cuore.fakhri@gmail.com>
2020-06-02 22:39:48 +0100

    BaseGeometry (based on 5-base_geometry.py).

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module class
+"""
+
+
+class BaseGeometry:
+    """
+    Improving Geometry class
+    """
+    def area(self):
+        """
+        Public instance method:raises an
+        Exception with the message area()
+        is not implemented
+        """
+        raise Exception("area() is not implemented")
