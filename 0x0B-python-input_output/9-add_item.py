663e16f568344b9f2b2bff366c320043a78cd457
wolf-coder <cuore.fakhri@gmail.com>
2020-06-04 20:19:22 +0100

    Fixing

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -3,17 +3,15 @@
 Module to manage file.
 """
 import sys
+
+
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
-
-
 filename = "add_item.json"
 Args = sys.argv[1:]
 try:
     Content = load_from_json_file(filename)
-except JSONDecodeError:
-    save_to_json_file([], filename)
+except FileNotFoundError:
     Content = []
-
 Added = Content + Args
 save_to_json_file(Added, "add_item.json")
