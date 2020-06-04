468fdf3339bd429429d49805f939cf69f72e4e4d
wolf-coder <cuore.fakhri@gmail.com>
2020-06-04 20:13:07 +0100

    9. Load, add, save

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module to manage file.
+"""
+import sys
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+
+filename = "add_item.json"
+Args = sys.argv[1:]
+try:
+    Content = load_from_json_file(filename)
+except JSONDecodeError:
+    save_to_json_file([], filename)
+    Content = []
+
+Added = Content + Args
+save_to_json_file(Added, "add_item.json")
