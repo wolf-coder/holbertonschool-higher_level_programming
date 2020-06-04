44942a44b8d92208a2372875cfbaeab403afbeb6
wolf-coder <cuore.fakhri@gmail.com>
2020-06-04 16:01:50 +0100

    fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,8 +6,8 @@ Module to read file
 
 def read_file(filename=""):
     """
-    function that reads a text file (UTF8)
+    function that reads  a text file (UTF8)
     and prints it to stdout.
     """
     with open(filename, "r") as f:
-        print(f.read())
+        print(f.read(), end="")
