c2ab3fbd23457f03efb34c242486160d825e5971
wolf-coder <cuore.fakhri@gmail.com>
2020-06-04 16:38:20 +0100

    pep8 fixing

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -10,6 +10,6 @@ def write_file(filename="", text=""):
     file (UTF8) and returns the number of
     characters written.
     """
-    
+
     with open(filename, "w") as f:
         return f.write(text)
