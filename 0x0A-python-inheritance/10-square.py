0c369d89374649da50b8f3c7704cf390fb2964b5
wolf-coder <cuore.fakhri@gmail.com>
2020-06-06 13:49:44 +0100

    10. Square #1

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module class that inherits from Rectangle
+"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """
+    Module Square ingerits form Rectangle.
+    """
+    def __init__(self, size):
+        """
+        initialisation of attributes
+        """
+        super().__init__(size, size)
