9deffad1c952ecab64f68f7a351c7ee8b799f47f
wolf-coder <cuore.fakhri@gmail.com>
2020-06-07 23:59:25 +0100

    12-student.py

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Module Class
+"""
+
+
+class Student:
+    """
+    Class Student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Instantiation constructor.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        retrieves a dictionary representation of a
+        Student instance.
+        """
+        if type(attrs) is list:
+            return {Atr: getattr(self, Atr)
+                    for Atr in attrs if Atr in
+                    self.__dict__.keys()}
+        else:
+            return self.__dict__
