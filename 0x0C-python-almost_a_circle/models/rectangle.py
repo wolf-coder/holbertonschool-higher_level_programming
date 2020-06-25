86dcde1b4e22cd2c0882c9a626817414cedeef38
wolf-coder <cuore.fakhri@gmail.com>
2020-06-25 19:04:06 +0100

    fixing docs

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,7 +6,11 @@ from models.base import Base
 
 
 class Rectangle(Base):
+    """
+    """
     def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        """
         super().__init__(id)
         self.__width = width
         self.__height = height
@@ -44,6 +48,8 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, x):
+        """
+        """
         self.__x = x
 
     @property
@@ -54,4 +60,6 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, y):
+        """
+        """
         self.__y = y
