7b55e0ab100f544f461e01ce2ab0477608da13ad
wolf-coder <cuore.fakhri@gmail.com>
2020-06-25 17:59:04 +0100

    Base file

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module
+"""
+class Base:
+    """Documentation for Base
+
+    """
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """
+        initialization constructor
+        """
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
