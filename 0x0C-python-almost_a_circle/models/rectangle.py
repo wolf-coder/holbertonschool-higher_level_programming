ed36cded68c2d5f56fb046d3306a1584c0f9a8b6
wolf-coder <cuore.fakhri@gmail.com>
2020-06-25 19:01:56 +0100

    models/rectangle.py

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""
+Module doc
+"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+
+    @property
+    def width(self):
+        """
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        """
+        """
+        self.__width = width
+
+    @property
+    def height(self):
+        """
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        """
+        """
+        self.__height = height
+
+    @property
+    def x(self):
+        """
+        """
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        self.__x = x
+
+    @property
+    def y(self):
+        """
+        """
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        self.__y = y
