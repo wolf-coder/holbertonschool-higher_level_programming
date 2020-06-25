ab396818755f3ed19bc96be587e6d53102504619
wolf-coder <cuore.fakhri@gmail.com>
2020-06-25 18:13:14 +0100

    fixing

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,16 +2,19 @@
 """
 Module
 """
+
+
 class Base:
     """Documentation for Base
 
     """
     __nb_objects = 0
+
     def __init__(self, id=None):
         """
         initialization constructor
         """
-        if id != None:
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
