d10948390c860bbd52054bb2a220b382423a46c1
wolf-coder <cuore.fakhri@gmail.com>
2020-08-24 22:34:17 +0100

    6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+6. POST an email #1
+"""
+from sys import argv
+import requests
+if __name__ == "__main__":
+    url = argv[1]
+    values = {"email": argv[2]}
+
+    RequestT = requests.post(url, values)
+    print(Request.text)
