14b52ab2a2869acac0dd33c7dde65b8303e2f55d
wolf-coder <cuore.fakhri@gmail.com>
2020-08-24 21:52:54 +0100

    fetching  https://intranet.hbtn.io/status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""[fetching  https://intranet.hbtn.io/status]
+"""
+from urllib import request
+if __name__ == "__main__":
+    with request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print('Body response:')
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode()))
