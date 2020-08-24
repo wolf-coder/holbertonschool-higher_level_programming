d5d98ef1bb6520c4eb0835fb9be7cd49f86d4820
wolf-coder <cuore.fakhri@gmail.com>
2020-08-24 23:23:03 +0100

    7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -2,10 +2,10 @@
 """
 """
 from sys import argv
-from  requests import get
+import  requests 
 if __name__ == "__main__":
     url = argv[1]
-    Request = get(argv[1])
+    Request = requests.get(argv[1])
     if Request.status_code <= 400:
         print(Request.text)
     else:
