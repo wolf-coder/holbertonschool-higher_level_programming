f1f829edc44875513added343953aa19537edff0
wolf-coder <cuore.fakhri@gmail.com>
2020-11-09 21:53:47 +0100

    Fix2

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -5,4 +5,5 @@ ALTER DATABASE hbtn_0c_0
 ALTER TABLE hbtn_0c_0.first_table
       CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE hbtn_0c_0.first_table
-      MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+      MODIFY name VARCHAR(100)
+      	     CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
