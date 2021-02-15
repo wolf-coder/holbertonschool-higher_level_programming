861b68f3ed89395f191cfe217dcbf052cc6bf1c9
wolf-coder <cuore.fakhri@gmail.com>
2021-02-15 11:33:29 +0100

    Comments

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -2,9 +2,9 @@
 
     -- Each record should display: tv_shows.title - tv_show_genres.genre_id
     -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+    -- If a show doesn’t have a genre, display NULL
     -- You can use only one SELECT statement
-    -- The database name will be passed as an argument of the mysql command
-
+    -- The database name will be passed as an argument of the mysql command.
 
 SELECT title, genre_id FROM tv_shows
 LEFT JOIN tv_show_genres
