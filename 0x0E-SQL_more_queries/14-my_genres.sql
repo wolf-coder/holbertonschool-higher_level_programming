805be6a7229fe3f560890dd5f4ad84aff5d87fad
wolf-coder <cuore.fakhri@gmail.com>
2021-02-15 15:07:16 +0100

    The tv_shows table contains only one record where title = Dexter (but the id can be different)

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,17 @@
+-- Write a script that uses the hbtn_0d_tvshows database to lists all genres of the show Dexter.
+
+    -- The tv_shows table contains only one record where title = Dexter (but the id can be different)
+    -- Each record should display: tv_genres.name
+    -- Results must be sorted in ascending order by the genre name
+    -- You can use only one SELECT statement
+    -- The database name will be passed as an argument of the mysql command
+
+SELECT name FROM tv_show_genres
+INNER JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+WHERE show_id =
+(
+ SELECT id FROM tv_shows
+ WHERE title = "Dexter"
+)
+ORDER BY name ASC;
