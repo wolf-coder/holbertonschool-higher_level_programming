f1983c37434112456f1e3e982ffb70dcc280d0d3
wolf-coder <cuore.fakhri@gmail.com>
2021-02-15 10:49:40 +0100

    All SQL keywords should be in uppercase.

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
 
-SELECT title, genre_id from tv_shows
+SELECT title, genre_id FROM tv_shows
 INNER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 ORDER BY
