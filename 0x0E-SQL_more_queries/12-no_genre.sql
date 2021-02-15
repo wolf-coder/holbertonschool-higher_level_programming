15eb54bc8fd6424994b97bedeac6ff6cfb15dbcb
wolf-coder <cuore.fakhri@gmail.com>
2021-02-15 11:50:52 +0100

    Fix

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -8,7 +8,5 @@
 SELECT title, genre_id FROM tv_shows
 LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
-WHERE genre_id = Nill
-ORDER BY
-      title ASC,
-      genre_id ASC;
+WHERE genre_id IS NULL
+ORDER BY title ASC, genre_id ASC;
