fcd27d4c7f6badf48cf866fbf941bc9797e40964
wolf-coder <cuore.fakhri@gmail.com>
2021-02-15 15:09:47 +0100

    Fix2

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -10,8 +10,6 @@ SELECT name FROM tv_show_genres
 INNER JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
 WHERE show_id =
-(
-SELECT id FROM tv_shows
-WHERE title = "Dexter"
-)
+(SELECT id FROM tv_shows
+WHERE title = "Dexter")
 ORDER BY name ASC;
