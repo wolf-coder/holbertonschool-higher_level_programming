ae5518f19943941bbad7c5559763bf3e89a9ea5e
wolf-coder <cuore.fakhri@gmail.com>
2021-05-13 23:54:07 +0100

    fix semistandard

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-const Tab = ['C is fun', 'Python is cool', 'JavaScript is amazing']
+const Tab = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i <= 2; i++) {
   console.log(Tab[i]);
 }
-
