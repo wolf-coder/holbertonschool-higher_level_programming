88fcdcc73e27426024d35be7dd4ec799bd96f768
wolf-coder <cuore.fakhri@gmail.com>
2021-05-25 16:46:24 +0100

    fix3

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  const listRev = list.reverse();
-  return listRev;
+  const ListRev = [];
+  for (let idx = list.length - 1; idx >= 0; idx--) {
+    ListRev.push(list[idx]);
+  }
+  return ListRev;
 };
