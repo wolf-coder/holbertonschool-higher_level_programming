8a71b15f5119f826295d1751c82d37e42378a73c
wolf-coder <cuore.fakhri@gmail.com>
2021-06-07 15:46:38 +0100

    fix pep

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """
-Write a function that prints a text with 2 new lines after each of these characters: ., ? and :
+Write a function that prints a text with 2 new lines
+after each of these characters: ., ? and :
 """
 
 
 def text_indentation(text):
     """
-    Write a function that prints a text with 2 new lines after each of these characters: ., ? and :
+    Write a function that prints a text with 2
+    new lines after each of these characters: ., ? and :
     """
 
     if not isinstance(text, str):
@@ -18,8 +20,8 @@ def text_indentation(text):
             continue
         else:
             if text[char] not in ".?:":
-                print(text[char], end = '')
+                print(text[char], end='')
                 ignore_space = False
             else:
-                print(text[char], end = '\n\n')
+                print(text[char], end='\n\n')
                 ignore_space = True
