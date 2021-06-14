89c110e5965b987e771ab91a5b89530c90ddd967
wolf-coder <cuore.fakhri@gmail.com>
2021-06-14 21:48:02 +0100

    fix0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-script that lists all states from the database hbtn_0e_0_usa
+script that lists all states from the database hbtn_0_0_usa
 Your script should take 3 arguments:
 mysql username, mysql password and database name
 (no argument validation needed)
@@ -13,9 +13,10 @@ username, password, database = sys.argv[1:]
 db = MySQLdb.connect(host="localhost", user=username,
                      passwd=password, db=database)
 cursor = db.cursor()
-sql = "SELECT * FROM states"
+sql = "SELECT * FROM states ORDER BY id ASC"
 cursor.execute(sql)
 query_rows = cursor.fetchall()
 
 for row in query_rows:
     print(row)
+db.close()
