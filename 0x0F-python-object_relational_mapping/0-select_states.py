648753219902323159971c8f70a75774999da64c
wolf-coder <cuore.fakhri@gmail.com>
2021-06-14 21:55:43 +0100

    fix3

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,23 +1,33 @@
 #!/usr/bin/python3
 """
-script that lists all states from the database hbtn_0_0_usa
-Your script should take 3 arguments:
-mysql username, mysql password and database name
-(no argument validation needed)
+Select from database
 """
 
 import sys
 import MySQLdb
-username, password, database = sys.argv[1:]
 
-db = MySQLdb.connect(host="localhost", user=username,
-                     passwd=password, db=database)
-cursor = db.cursor()
-sql = "SELECT * FROM states ORDER BY id ASC"
-cursor.execute(sql)
-query_rows = cursor.fetchall()
 
-for row in query_rows:
-    print(row)
-cursor.close()
-db.close()
+def Selector():
+    """
+    script that lists all states from the database hbtn_0_0_usa
+    Your script should take 3 arguments:
+    mysql username, mysql password and database name
+    (no argument validation needed)
+    """
+    username, password, database = sys.argv[1:]
+
+    db = MySQLdb.connect(host="localhost", user=username,
+                         passwd=password, db=database)
+    cursor = db.cursor()
+    sql = "SELECT * FROM states ORDER BY id ASC"
+    cursor.execute(sql)
+    query_rows = cursor.fetchall()
+
+    for row in query_rows:
+        print(row)
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    Selector()
