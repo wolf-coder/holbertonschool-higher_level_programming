48d99265bfe0b96134d131db851e7304cecca688
wolf-coder <cuore.fakhri@gmail.com>
2021-06-18 20:08:26 +0100

    first row only

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Fetching table rows mode
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+
+def Fetching(*kw):
+
+    """
+    Method to list all State objects from the database hbtn_0e_6_usa
+    """
+
+    """
+    1) Connecting to DataBase
+    """
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(*kw[0]),
+                           pool_pre_ping=True)
+    """
+    2) Create Session
+    """
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    """
+    3)Quering
+    """
+    for id, name in session.query(State.id, State.name)[:1]:
+        print("{}: {}".format(id, name))
+
+
+if __name__ == "__main__":
+    Fetching(sys.argv[1:])
