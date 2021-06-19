8fe205c1ef4b4a2a02a2c388e1d06e42b7f9497a
wolf-coder <cuore.fakhri@gmail.com>
2021-06-19 16:32:54 +0100

    fix4

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -15,12 +15,13 @@ def Fetching(*kw):
     Write a script that prints the State object with the name passed
     as argument from the database hbtn_0e_6_usa.
     """
+    username, password, database, searched = kw[0]
 
     """
     1) Connecting to DataBase
     """
-    *login, searched = *kw[0],
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(*login),
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(username, password, database),
                            pool_pre_ping=True)
     """
     2) Create Session
@@ -39,6 +40,7 @@ def Fetching(*kw):
     else:
         print("Not found")
     session.close()
+
 
 if __name__ == "__main__":
     Fetching(sys.argv[1:])
