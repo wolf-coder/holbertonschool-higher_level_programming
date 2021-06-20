8eeffc707826219975a46b6fe9297669ac04e2fc
wolf-coder <cuore.fakhri@gmail.com>
2021-06-20 22:35:05 +0100

    adding row

---
+++ 0x0F-python-object_relational_mapping/11.model_state_insert.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Write a script that adds the State object “Louisiana” to
+the database hbtn_0e_6_usa
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+
+def Fetching(*kw):
+
+    """
+    Write a script that adds the State object “Louisiana” to
+    the database hbtn_0e_6_usa
+    """
+    username, password, database = kw[0]
+
+    """
+    1) Connecting to DataBase
+    """
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(username, password, database),
+                           pool_pre_ping=True)
+    """
+
+    2) Create Session
+    """
+    Session = sessionmaker()
+    session = Session(bind=engine)
+
+    """
+    3)Adding state
+    """
+    Added_row = State(name='Louisiana')
+    session.add(Added_row)
+    session.commit()
+    session.close()
+
+
+if __name__ == "__main__":
+    Fetching(sys.argv[1:])
