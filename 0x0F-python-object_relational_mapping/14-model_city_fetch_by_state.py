d08431e47e9ee97980aac7b79341dc40468de767
wolf-coder <cuore.fakhri@gmail.com>
2021-06-23 15:42:06 +0100

    Class definition of a State and an instance Base = declarative_base()

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Write a script that adds the State object “Louisiana” to
+the database hbtn_0e_6_usa
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model_city import city
+
+
+def Display_by_CitiesID(*kw):
+
+    """
+    Write a script that adds the State object “Louisiana” to
+    the database hbtn_0e_6_usa
+    """
+    username, password, database = kw[0]
+
+    """
+    1) Connecting to DataBase
+    """
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(username, password, database),
+                           pool_pre_ping=True)
+    """
+
+    2) Create Session
+    """
+    Session = sessionmaker()
+    session = Session(bind=engine)
+
+    """
+    3)Display by Cities Ids
+    """
+    for c, s in session.query(city, State).filter(city.state_id ==
+                                                  State.id).all():
+        print("{}: ({}) {}".format(s.name, c.id, c.name))
+
+
+if __name__ == "__main__":
+    Display_by_CitiesID(sys.argv[1:])
