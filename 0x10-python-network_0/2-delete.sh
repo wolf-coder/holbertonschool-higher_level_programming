dd444742ae8c60855acf602b1a8830307dc71d56
wolf-coder <cuore.fakhri@gmail.com>
2021-06-24 21:32:43 +0100

    fix4

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#Write a Bash script that sends a DELETE request to the URL passed as the first argument and displays the body of the response
+#Bash script that sends a DELETE request to the URL
 curl -s "$1" -X DELETE
