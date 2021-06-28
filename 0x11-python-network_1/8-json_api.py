15472775ff6e76815c140e457c94ead94882948d
wolf-coder <cuore.fakhri@gmail.com>
2021-06-28 17:18:15 +0100

    dix0

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -3,14 +3,15 @@
 handle json
 """
 import requests
-from sys import argv
+import sys
 if __name__ == "__main__":
-    if len(argv) > 1:
-        q = argv[1]
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
     else:
         q = ""
     value = {"q": q}
-    url = "http://0.0.0.0:5000/search_user"
+    # url = "http://0.0.0.0:5000/search_user"
+    url = "http://34.227.194.97:50029/search_user"
     r = requests.post(url, value)
     try:
         json_data = r.json()
