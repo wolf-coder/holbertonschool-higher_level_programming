397e91b40b50a942a2fe19a9ac6f32736533f734
wolf-coder <cuore.fakhri@gmail.com>
2021-06-28 16:17:48 +0100

    fix1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """
+Handling error code
 """
 import requests
 from sys import argv
 if __name__ == "__main__":
     url = argv[1]
-    REQUEST = requests.get(argv[1])
-    if REQUEST.status_code >= 400:
-        print(REQUEST.text)
+    Resp = requests.get(argv[1])
+    if Resp.status_code <= 400:
+        print(Resp.text)
     else:
-        print("Error code: {}".format(REQUEST.status_code))
+        print("Error code: {}".format(Resp.status_code))
