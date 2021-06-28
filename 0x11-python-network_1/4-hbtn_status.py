f184b3d669d59d19583dc2ee8bf70aeb179dee25
wolf-coder <cuore.fakhri@gmail.com>
2021-06-28 01:17:57 +0100

    fix0

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
 """
- 4. What's my status? 
+4. What's my status?
 """
 
 import requests
 if __name__ == "__main__":
     url = "https://intranet.hbtn.io/status"
     Request = requests.get(url)
-    Request = Request.text
+    body = Request.text
     print("Body response:")
-    print("\t- type: {}".format(type(Request)))
-    print("\t- content: {}".format(Request))
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
