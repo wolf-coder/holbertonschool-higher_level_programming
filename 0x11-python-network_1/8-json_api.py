4173779023f28cbb0448188f468833e613bfcb81
wolf-coder <cuore.fakhri@gmail.com>
2021-06-28 17:24:17 +0100

    fix2

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,10 +9,10 @@ if __name__ == "__main__":
         q = sys.argv[1]
     else:
         q = ""
-    value = {"q": q}
+    values = {"q": q}
     url = "http://0.0.0.0:5000/search_user"
     # url = "http://34.227.194.97:50029/search_user"
-    r = requests.post(url, value)
+    r = requests.post(url, data=values)
     try:
         json_data = r.json()
         if json_data == {}:
