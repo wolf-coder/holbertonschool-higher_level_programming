53a78f2a6ab4e2421ebd804eb181eee7b8374eb2
wolf-coder <cuore.fakhri@gmail.com>
2021-06-30 18:35:23 +0100

    sdf

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -39,7 +39,7 @@ def Display_by_CitiesID(*kw):
     session.add(newstate)
     session.add(newcity)
     session.commit()
-
+    session.close()
 
 if __name__ == "__main__":
     Display_by_CitiesID(sys.argv[1:])
