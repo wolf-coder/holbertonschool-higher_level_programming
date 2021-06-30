184e4fb210169c19bc9ae6cc955bd149b2c6b38c
wolf-coder <cuore.fakhri@gmail.com>
2021-06-30 16:53:31 +0100

    fix5

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -2,8 +2,9 @@
 """
 Class definition of a State and an instance Base = declarative_base()
 """
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String
 from model_state import Base
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
 
 
@@ -13,10 +14,6 @@ class City(Base):
     """
 
     __tablename__ = 'cities'
-    id = Column(Integer,
-                primary_key=True,
-                autoincrement=True,
-                unique=True,
-                nullable=False)
+    id = Column(Integer(), primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state_id = Column(Integer(), ForeignKey('states.id'))
