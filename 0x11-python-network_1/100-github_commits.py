87d24b319190242b468a86ab21cefbf0446bfe2b
wolf-coder <cuore.fakhri@gmail.com>
2021-06-30 02:50:09 +0100

    fix2

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -3,19 +3,15 @@
 Python script that takes 2 arguments in order to display last 10 commits.
 """
 
-
 if __name__ == '__main__':
     import sys
     import requests
     repo = sys.argv[1]
     owner = sys.argv[2]
-    url = '/'.join(["https://api.github.com/repos",
-                    repo,
-                    owner,
-                    "commits"])
-    Resp = requests.get(url)
-    Commits = Resp.json()[:10]
-    for i in Commits:
+    info = owner + "/" + repo
+    r = requests.get("https://api.github.com/repos/" + info + "/commits")
+    cmt = r.json()[:10]
+    for i in cmt:
         elem = i['sha']
         author = i['commit']['author']['name']
         print('{}: {}'.format(elem, author))
