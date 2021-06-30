8fb41015f6c99416f92fb01a39e59c4b2ad9bb62
wolf-coder <cuore.fakhri@gmail.com>
2021-06-30 18:58:07 +0100

    sdklfsj

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -23,8 +23,8 @@ def Display_by_CitiesID(*kw):
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
                            format(username, password, database),
                            pool_pre_ping=True)
-    """
 
+    """
     2) Create Session
     """
     Base.metadata.create_all(engine)
@@ -39,6 +39,6 @@ def Display_by_CitiesID(*kw):
     session.commit()
     session.close()
 
-
+    
 if __name__ == "__main__":
     Display_by_CitiesID(sys.argv[1:])
