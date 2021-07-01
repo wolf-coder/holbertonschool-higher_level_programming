671add6cfe183f7a3255093ba81413d5484e70c6
wolf-coder <cuore.fakhri@gmail.com>
2021-07-01 22:55:36 +0100

    101-relationship_states_cities_list.py

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Write a script that adds the State object “Louisiana” to
+the database hbtn_0e_6_usa
+"""
+import sys
+from relationship_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from relationship_city import City
+
+
+def Display_by_CitiesID(*kw):
+    """
+    Write a script that adds the State object “Louisiana” to
+    the database hbtn_0e_6_usa
+    """
+    username, password, database = kw[0]
+    """
+    1) Connecting to DataBase
+    """
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(username, password, database),
+                           pool_pre_ping=True)
+
+    """
+    2) Create Session
+    """
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    """
+    3)Display by Cities Ids
+    """
+    for S in session.query(State).all():
+        print("{}: {}".format(S.id, S.name))
+        for C in S.cities:
+            print("    {}: {}".format(C.id, C.name))
+    session.close()
+
+
+if __name__ == "__main__":
+    Display_by_CitiesID(sys.argv[1:])
